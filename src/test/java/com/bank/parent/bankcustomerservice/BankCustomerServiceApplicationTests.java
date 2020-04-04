8b348dfff417ac17dcd41437b076f31b2d2910da
saurabh5788 <saurabh.singh5788@gmail.com>
2020-04-04 20:20:14 +0530

    First COmmit

---
+++ src/test/java/com/bank/parent/bankcustomerservice/BankCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.parent.bankcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
