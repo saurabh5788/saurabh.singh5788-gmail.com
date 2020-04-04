3802a345edcbc50ad7b845b1d1e49b13b7400355
saurabh5788 <saurabh.singh5788@gmail.com>
2020-04-04 20:34:28 +0530

    Controller Added

---
+++ src/main/java/com/bank/parent/bankcustomerservice/controller/BankCustomerServiceController.java
@@ -0,0 +1,14 @@
+package com.bank.parent.bankcustomerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/bank-customer-service")
+public class BankCustomerServiceController {
+	@GetMapping(path = "/hello")
+	public String helloParty() {
+		return "Hello Bank Customer!!!";
+	}
+}
