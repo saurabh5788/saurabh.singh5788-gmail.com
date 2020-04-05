68c7663d4a1963719143e07d442b5af3d8487ec6
saurabh5788 <saurabh.singh5788@gmail.com>
2020-04-05 23:34:50 +0530

    More Loggers

--- src/main/java/com/bank/parent/bankcustomerservice/BankCustomerServiceApplication.java
+++ src/main/java/com/bank/parent/bankcustomerservice/BankCustomerServiceApplication.java
@@ -1,13 +1,65 @@
 package com.bank.parent.bankcustomerservice;
 
+import java.util.Arrays;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class BankCustomerServiceApplication {
+public class BankCustomerServiceApplication implements CommandLineRunner,
+		ApplicationRunner {
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(BankCustomerServiceApplication.class.getName());
+	@Value("${spring.application.name}")
+	private String applicationName;
+	@Value("${server.port}")
+	private Integer serverPort;
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankCustomerServiceApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(
+				BankCustomerServiceApplication.class, args);
+		Integer noOfBeans =  ctx.getBeanDefinitionCount();
+		LOGGER.info("Total Beans : {}", noOfBeans);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+
+		Arrays.sort(beanNames);
+
+		for (String beanName : beanNames) {
+			LOGGER.debug(beanName);
+		}
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		LOGGER.info("Inside ApplicationRunner");
+		LOGGER.debug("Application Name : {}", applicationName);
+		LOGGER.debug("Server Port : {}", serverPort);
+		Set<String> optionNameSet = args.getOptionNames();
+		int optionNameIndex = 0;
+		for (String optionName : optionNameSet) {
+			LOGGER.debug("Argument ({}) : {}", ++optionNameIndex, optionName);
+		}
+		LOGGER.info("Exit ApplicationRunner");
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("Inside CommandLineRunner");
+		LOGGER.debug("Application Name : {}", applicationName);
+		LOGGER.debug("Server Port : {}", serverPort);
+		int argIndex = 0;
+		for (String arg : args) {
+			LOGGER.debug("Argument ({}) : {}", ++argIndex, arg);
+		}
+		LOGGER.info("Exit CommandLineRunner");
+	}
 }
--- src/main/java/com/bank/parent/bankcustomerservice/controller/BankCustomerServiceController.java
+++ src/main/java/com/bank/parent/bankcustomerservice/controller/BankCustomerServiceController.java
@@ -1,5 +1,7 @@
 package com.bank.parent.bankcustomerservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/bank-customer-service")
 public class BankCustomerServiceController {
+	private final static Logger LOGGER = LoggerFactory.getLogger(BankCustomerServiceController.class.getName());
 	@GetMapping(path = "/hello")
-	public String helloParty() {
+	public String helloBankCustomer() {
+		LOGGER.info("Processing Hello Bank Customer!!!");
 		return "Hello Bank Customer";
 	}
 }
